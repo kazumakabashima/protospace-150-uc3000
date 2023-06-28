57b3f2a8f5d582fcb0f91be944407ea1c729ead5
kazumakabashima <k.kazuma1028@gmail.com>
2023-06-28 14:45:16 +0900

    アクティブストレージ実装
    アソシエーション、バリデーション実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :user_name, :profile, :affiliation, :job_title, presence: true
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+
+  has_many :prototypes
+  has_many :comments
 end
